5b1dca580ea615b4633304f96ed26542f2a55df1
smasson99 <smasson99@gmail.com>
2017-08-10 12:55:47 -0400

    Update

---
+++ CompleteAlpha_PrototypeTesting/Assets/Scripts/Actors/Research02/DestroyableEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Scripts/Entitys/DestroyableObject")]
+public class DestroyableEntity : MonoBehaviour
+{
+    #region:components
+    private Team team;
+
+    public Team.TeamName Team
+    {
+        get { return team.CurrentTeam; }
+        set
+        {
+            team.CurrentTeam = value;
+        }
+    }
+    private Stats stats;
+    #endregion
+}
